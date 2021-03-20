c7a122d20aa5be1f3fe09f09636fc31cf7f544cc
Serhii Bura <serhiibura@gmail.com>
2021-03-21 00:13:53 +0200

    communication: common: Add communication message structure
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

---
+++ communication/common/inc/message.hpp
@@ -0,0 +1,49 @@
+/**@file	message.hpp
+ *
+ * @desrc Communication interface message
+ *
+ * @authors	
+ *      Serhii Bura <serhiibura@gmail.com>
+ */
+
+#ifndef _MESSAGE_HPP_
+#define _MESSAGE_HPP_
+
+#include <cstdint>
+
+typedef enum : std::uint8_t {
+    eCommandMessage     = 0,
+    eRequestMessage     = 1,
+    eResponceMessage    = 2,
+    eAcknowledgeMessage = 3
+} message_type_t;
+
+typedef enum : std::uint8_t {
+    eResponceSuccess       = 0,
+    eResponceCRCError      = 1,
+    eResponceSequenceError = 2,
+    eResponceUnknownError  = 0xF
+} message_reponce_code_t;
+
+#pragma pack(push, 1)
+
+typedef struct message_header_st {
+    std::uint8_t msg_id;
+    std::uint8_t src_id;
+    std::uint8_t dst_id;
+    std::uint8_t msg_type : 2;
+    std::uint8_t retry : 1;
+    std::uint8_t sequence : 1;
+    std::uint8_t resp_code : 4;
+    std::uint16_t data_length;
+} message_header_st_t;
+
+typedef struct message_st {
+    message_header_st_t header;
+    std::uint8_t *data_p;
+    std::uint32_t crc;
+} message_st_t;
+
+#pragma pack(pop)
+
+#endif /*_MESSAGE_HPP_*/
